f79cfcf7df44243ef4653690fe9b5dcd14a45573
Zali Rademaekers <qwf5520@autuni.ac.nz>
2024-05-10 17:16:32 +1200

    BodyDouble simplified
    using updateDB from DBManager

--- src/BodyDoublerOO/BodyDouble.java
+++ src/BodyDoublerOO/BodyDouble.java
@@ -38,7 +38,7 @@ public class BodyDouble {
             this.statement.executeUpdate("CREATE TABLE ADMIN (ADMINID VARCHAR(6), "
                     + "PASSWORD VARCHAR(8), FIRSTNAME VARCHAR(20), LASTNAME VARCHAR(20), "
                     + "EMAIL VARCHAR(40), ROLE VARCHAR(20), DEPARTMENT VARCHAR(30))");
-            System.out.println("Table ADMIN created!");
+            System.out.println("Admin table created!");
         } catch (SQLException ex) {
             Logger.getLogger(BodyDouble.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -46,67 +46,27 @@ public class BodyDouble {
     
     public void createMemberTable() {
         
-        try {
-            this.checkExists("MEMBER");
-            this.statement = conn.createStatement();
-            
-            this.statement.executeUpdate("CREATE TABLE MEMBER (MEMBERID VARCHAR(6), "
-                    + "PASSWORD VARCHAR(8), FIRSTNAME VARCHAR(20), LASTNAME VARCHAR(20), "
-                    + "EMAIL VARCHAR(40), STUDENTID INT, DEGREESPROGRAM VARCHAR(4), AGE INT)");
-            System.out.println("Table MEMBER created");
-            //statement.executeUpdate("SELECT * FROM PROMOTION");
-        } catch (SQLException ex) {
-            Logger.getLogger(BodyDouble.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        this.checkExists("MEMBER");
+        this.dbManager.updateDB("CREATE TABLE MEMBER (MEMBERID VARCHAR(6), "
+                + "PASSWORD VARCHAR(8), FIRSTNAME VARCHAR(20), LASTNAME VARCHAR(20), "
+                + "EMAIL VARCHAR(40), STUDENTID INT, DEGREESPROGRAM VARCHAR(4), AGE INT)");
+        System.out.println("Memeber table created");
     }
     
     public void createSessionTable() {
         
-        try {
-            this.checkExists("SESSION");
-            this.statement = conn.createStatement();
-            
-            this.statement.executeUpdate("CREATE TABLE SESSION (SESSIONID VARCHAR(6), "
-                    + "DATE DATE, TIME TIME, ROOM VARCHAR(20), "
-                    + "MAXPEOPLE INT, ACTUALPEOPLE INT)");
-            System.out.println("Table SESSION created");
-        } catch (SQLException ex) {
-            Logger.getLogger(BodyDouble.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        this.checkExists("SESSION");
+        this.dbManager.updateDB("CREATE TABLE SESSION (SESSIONID VARCHAR(6), "
+                + "DATE DATE, TIME TIME, ROOM VARCHAR(20), "
+                + "MAXPEOPLE INT, ACTUALPEOPLE INT)");
+        System.out.println("Session table created");
     }
     
-    public ResultSet getWeekSpecial() {
-        ResultSet rs = null;
-        try {
-            rs = this.statement.executeQuery("SELECT TITLE, PRICE, DISCOUNT FROM BOOK, PROMOTION WHERE BOOK.CATEGORY=PROMOTION.CATEGORY");
-        } catch (SQLException ex) {
-            Logger.getLogger(BodyDouble.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return (rs);
+    public void createRoomTable() {
         
-    }
-    
-    public void createWeekSpecialTable(ResultSet rs) {
-        
-        try {
-            this.checkExists("WEEKSPECIAL");
-            this.statement = conn.createStatement();
-            this.statement.executeUpdate("CREATE TABLE WEEKSPECIAL (TITLE VARCHAR(50), SPECIALPRICE FLOAT)");
-            System.out.println("Table WEEKSPECIAL created!");
-            while (rs.next()) {
-                String title = rs.getString("TITLE");
-                float price = rs.getFloat("PRICE");
-                int discount = rs.getInt("DISCOUNT");
-                float newPrice = price - (100 - discount) / 100;
-                this.statement.addBatch("INSERT INTO WEEKSPECIAL VALUES ('" + title + "', " + newPrice + ")");
-            }
-            this.statement.executeBatch();
-            System.out.println("Data inserted!");
-            rs.close();
-        } catch (SQLException ex) {
-            Logger.getLogger(BodyDouble.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
+        this.checkExists("ROOM");
+        this.dbManager.updateDB("CREATE TABLE ROOM (ROOMID VARCHAR(6), ROOMNAME VARCHAR(20)");
+        System.out.println("Room table created");
     }
     
     public void checkExists(String table) {
@@ -136,4 +96,39 @@ public class BodyDouble {
     public void closeConnection(){
         this.dbManager.closeConnections();
     }
+//    public ResultSet getWeekSpecial() {
+//        ResultSet rs = null;
+//        try {
+//            rs = this.statement.executeQuery("SELECT TITLE, PRICE, DISCOUNT FROM BOOK, PROMOTION WHERE BOOK.CATEGORY=PROMOTION.CATEGORY");
+//        } catch (SQLException ex) {
+//            Logger.getLogger(BodyDouble.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        return (rs);
+//        
+//    }
+    
+//    public void createWeekSpecialTable(ResultSet rs) {
+//        
+//        try {
+//            this.checkExists("WEEKSPECIAL");
+//            this.statement = conn.createStatement();
+//            this.statement.executeUpdate("CREATE TABLE WEEKSPECIAL (TITLE VARCHAR(50), SPECIALPRICE FLOAT)");
+//            System.out.println("Table WEEKSPECIAL created!");
+//            while (rs.next()) {
+//                String title = rs.getString("TITLE");
+//                float price = rs.getFloat("PRICE");
+//                int discount = rs.getInt("DISCOUNT");
+//                float newPrice = price - (100 - discount) / 100;
+//                this.statement.addBatch("INSERT INTO WEEKSPECIAL VALUES ('" + title + "', " + newPrice + ")");
+//            }
+//            this.statement.executeBatch();
+//            System.out.println("Data inserted!");
+//            rs.close();
+//        } catch (SQLException ex) {
+//            Logger.getLogger(BodyDouble.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        
+//    }
+    
+    
 }
