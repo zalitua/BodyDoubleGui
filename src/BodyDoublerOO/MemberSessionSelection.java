2881370b778ad83ea71596da8ae7761e4c11b2bc
Georgia Rouse <rsh2777@autuni.ac.nz>
2024-05-17 10:55:24 +1200

    member reg

--- src/BodyDoublerOO/MemberSessionSelection.java
+++ src/BodyDoublerOO/MemberSessionSelection.java
@@ -101,7 +101,7 @@ public class MemberSessionSelection extends javax.swing.JFrame {
                     .addComponent(joinSessionJButton)
                     .addComponent(sessionIDJComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(sessionIDJL))
-                .addContainerGap(166, Short.MAX_VALUE))
+                .addContainerGap(56, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
