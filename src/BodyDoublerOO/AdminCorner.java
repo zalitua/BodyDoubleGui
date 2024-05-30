33ce56f5ad44c876b9ce57e3176125a6584edcb4
Zali Rademaekers <qwf5520@autuni.ac.nz>
2024-05-31 10:20:45 +1200

    admin corner redesign

--- src/BodyDoublerOO/AdminCorner.java
+++ src/BodyDoublerOO/AdminCorner.java
@@ -64,17 +64,16 @@ public class AdminCorner extends javax.swing.JFrame {
     private void initComponents() {
 
         welcomeL = new javax.swing.JLabel();
-        editDeleteInstructJLabel = new javax.swing.JLabel();
-        sessionsJLabel = new javax.swing.JLabel();
+        addLabel = new javax.swing.JLabel();
         sessionIDJComboB = new javax.swing.JComboBox<>();
-        editSessionButton = new javax.swing.JButton();
-        deleteButton = new javax.swing.JButton();
+        editButton = new javax.swing.JButton();
         adminSessionJScroll = new javax.swing.JScrollPane();
         jScrollPane2 = new javax.swing.JScrollPane();
         sessionJTable = new javax.swing.JTable();
-        addRoomButton = new javax.swing.JButton();
-        deleteRoomButton = new javax.swing.JButton();
+        manageRoomsButton = new javax.swing.JButton();
         addButton = new javax.swing.JButton();
+        editLabel = new javax.swing.JLabel();
+        manageLabel = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setPreferredSize(new java.awt.Dimension(780, 440));
@@ -83,13 +82,9 @@ public class AdminCorner extends javax.swing.JFrame {
         welcomeL.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         welcomeL.setText("Welcome to the Admin Corner");
 
-        editDeleteInstructJLabel.setFont(new java.awt.Font("Georgia", 1, 14)); // NOI18N
-        editDeleteInstructJLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        editDeleteInstructJLabel.setText("To delete or edit a session, pick the session ID and then click the buttion you want to apply");
-
-        sessionsJLabel.setFont(new java.awt.Font("Georgia", 1, 18)); // NOI18N
-        sessionsJLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        sessionsJLabel.setText("Session ID");
+        addLabel.setFont(new java.awt.Font("Georgia", 1, 16)); // NOI18N
+        addLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        addLabel.setText("Add a session:");
 
         sessionIDJComboB.setFont(new java.awt.Font("Georgia", 1, 18)); // NOI18N
         sessionIDJComboB.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "SES007", "SES008", "SES006" }));
@@ -99,19 +94,11 @@ public class AdminCorner extends javax.swing.JFrame {
             }
         });
 
-        editSessionButton.setFont(new java.awt.Font("Georgia", 1, 18)); // NOI18N
-        editSessionButton.setText("Edit Session");
-        editSessionButton.addActionListener(new java.awt.event.ActionListener() {
+        editButton.setFont(new java.awt.Font("Georgia", 1, 16)); // NOI18N
+        editButton.setText("Edit");
+        editButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                editSessionButtonActionPerformed(evt);
-            }
-        });
-
-        deleteButton.setFont(new java.awt.Font("Georgia", 1, 18)); // NOI18N
-        deleteButton.setText("Delete Session");
-        deleteButton.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                deleteButtonActionPerformed(evt);
+                editButtonActionPerformed(evt);
             }
         });
 
@@ -128,109 +115,101 @@ public class AdminCorner extends javax.swing.JFrame {
 
         adminSessionJScroll.setViewportView(jScrollPane2);
 
-        addRoomButton.setFont(new java.awt.Font("Georgia", 1, 18)); // NOI18N
-        addRoomButton.setText("Add Room");
-
-        deleteRoomButton.setFont(new java.awt.Font("Georgia", 1, 18)); // NOI18N
-        deleteRoomButton.setText("Delete Room");
-        deleteRoomButton.addActionListener(new java.awt.event.ActionListener() {
+        manageRoomsButton.setFont(new java.awt.Font("Georgia", 1, 16)); // NOI18N
+        manageRoomsButton.setText("Manage Rooms");
+        manageRoomsButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                deleteRoomButtonActionPerformed(evt);
+                manageRoomsButtonActionPerformed(evt);
             }
         });
 
-        addButton.setFont(new java.awt.Font("Georgia", 1, 18)); // NOI18N
-        addButton.setText("Add Session");
+        addButton.setFont(new java.awt.Font("Georgia", 1, 16)); // NOI18N
+        addButton.setText("Add");
         addButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 addButtonActionPerformed(evt);
             }
         });
 
+        editLabel.setFont(new java.awt.Font("Georgia", 1, 16)); // NOI18N
+        editLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        editLabel.setText("Edit or delete a session:");
+
+        manageLabel.setFont(new java.awt.Font("Georgia", 1, 16)); // NOI18N
+        manageLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        manageLabel.setText("View and manage room:");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(welcomeL, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(adminSessionJScroll, javax.swing.GroupLayout.PREFERRED_SIZE, 751, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(0, 0, Short.MAX_VALUE)))
-                        .addContainerGap())
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
-                        .addComponent(sessionsJLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(59, 59, 59)
-                        .addComponent(sessionIDJComboB, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(280, 280, 280))))
+                        .addComponent(welcomeL, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(201, 201, 201))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(manageLabel)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(manageRoomsButton, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(200, 200, 200))))
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(editDeleteInstructJLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 790, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(118, 118, 118)
-                        .addComponent(deleteButton, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(39, 39, 39)
-                        .addComponent(editSessionButton)
-                        .addGap(42, 42, 42)
-                        .addComponent(addButton, javax.swing.GroupLayout.PREFERRED_SIZE, 178, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(0, 0, Short.MAX_VALUE))
-            .addGroup(layout.createSequentialGroup()
-                .addGap(190, 190, 190)
-                .addComponent(addRoomButton, javax.swing.GroupLayout.PREFERRED_SIZE, 157, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(61, 61, 61)
-                .addComponent(deleteRoomButton, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(sessionIDJComboB, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                            .addContainerGap()
+                            .addComponent(adminSessionJScroll, javax.swing.GroupLayout.PREFERRED_SIZE, 751, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(layout.createSequentialGroup()
+                            .addGap(120, 120, 120)
+                            .addComponent(addLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                            .addComponent(addButton, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGap(26, 26, 26)
+                            .addComponent(editLabel)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                            .addComponent(editButton, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(43, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(welcomeL, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
+                .addComponent(welcomeL, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(28, 28, 28)
                 .addComponent(adminSessionJScroll, javax.swing.GroupLayout.PREFERRED_SIZE, 107, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(editDeleteInstructJLabel)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(sessionIDJComboB, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(sessionsJLabel))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(editSessionButton)
-                    .addComponent(deleteButton)
-                    .addComponent(addButton))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(addRoomButton)
-                    .addComponent(deleteRoomButton))
-                .addContainerGap(109, Short.MAX_VALUE))
+                    .addComponent(addLabel)
+                    .addComponent(addButton)
+                    .addComponent(editLabel)
+                    .addComponent(editButton))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(manageLabel)
+                    .addComponent(manageRoomsButton)
+                    .addComponent(sessionIDJComboB, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(36, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void deleteButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_deleteButtonActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_deleteButtonActionPerformed
 
     private void sessionIDJComboBActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_sessionIDJComboBActionPerformed
         //action to delete a session
 
     }//GEN-LAST:event_sessionIDJComboBActionPerformed
 
-    private void editSessionButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_editSessionButtonActionPerformed
+    private void editButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_editButtonActionPerformed
         // open to EditSessionGUI
         EditSessionGUI edit = new EditSessionGUI();
         edit.setVisible(true);
-    }//GEN-LAST:event_editSessionButtonActionPerformed
+    }//GEN-LAST:event_editButtonActionPerformed
 
-    private void deleteRoomButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_deleteRoomButtonActionPerformed
+    private void manageRoomsButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_manageRoomsButtonActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_deleteRoomButtonActionPerformed
+    }//GEN-LAST:event_manageRoomsButtonActionPerformed
 
     private void addButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addButtonActionPerformed
         // open to AddEsssionGUI
@@ -275,16 +254,15 @@ public class AdminCorner extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton addButton;
-    private javax.swing.JButton addRoomButton;
+    private javax.swing.JLabel addLabel;
     private javax.swing.JScrollPane adminSessionJScroll;
-    private javax.swing.JButton deleteButton;
-    private javax.swing.JButton deleteRoomButton;
-    private javax.swing.JLabel editDeleteInstructJLabel;
-    private javax.swing.JButton editSessionButton;
+    private javax.swing.JButton editButton;
+    private javax.swing.JLabel editLabel;
     private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JLabel manageLabel;
+    private javax.swing.JButton manageRoomsButton;
     private javax.swing.JComboBox<String> sessionIDJComboB;
     private javax.swing.JTable sessionJTable;
-    private javax.swing.JLabel sessionsJLabel;
     private javax.swing.JLabel welcomeL;
     // End of variables declaration//GEN-END:variables
 }
